7ce78caf31fb2f78ff00e8f5eb803e40fda51386
saiyashwant07 <55803021+saiyashwant07@users.noreply.github.com>
2021-02-04 10:31:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import "./index.css"
+function App() {
+    return (
+        <div>
+            <div className="main">
+            <div className="top">HELLO</div>
+                <div className="inner-main"></div>
+             </div>    
+        </div>
+    )
+}
+
+export default App
